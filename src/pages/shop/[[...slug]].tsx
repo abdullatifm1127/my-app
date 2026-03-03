97504f005fafd40a02b768b7aa46222ce3694c5e
latif <mabdullatifm1127@gmail.com>
2026-03-03 17:49:14 +0700

    Langkah 5

--- src/pages/shop/[[...slug]].tsx
+++ src/pages/shop/[[...slug]].tsx
@@ -7,8 +7,9 @@ const HalamanToko = () => {
     return (
         <div>
             <h1> Halaman Toko</h1>
-            <p>Toko: { `${query.slug && query.slug[0]+"-"+ query.slug[1]}`}</p>
-            {/* <p>Toko: {Array.isArray(query.slug) ? query.slug.join("-") : query.slug}</p> */}
+            {/* <p>Toko: { `${query.slug && query.slug[0]+"-"+ query.slug[1]}`}</p> */}
+            <p>Toko: {Array.isArray(query.slug) ? query.slug.join("-") : query.slug}</p>
+            <p>Kategori: {query.slug ? query.slug[0]: "Semua Kategori"}</p>
         </div>
     );
 };
