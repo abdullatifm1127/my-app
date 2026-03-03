35c4ae9fa2a7c85eea01272c3547b06c3e1f7f0b
latif <mabdullatifm1127@gmail.com>
2026-03-03 21:51:46 +0700

    Tugas2

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -2,17 +2,19 @@ import Link from "next/link";
     import { useRouter } from "next/router";
 
 const halamanLogin = () => {
-    const {push} = useRouter();
+    const router = useRouter();
     const handleLogin = () => {
-        push("/produk");
+        console.log("Button klik, navigating to /produk");
+        localStorage.setItem("isLogin", "true");
+        router.push("/produk");
     }
     return (
         <div>
             <h1>Halaman Login</h1>
-            <button onClick={handleLogin}>Login</button> <br />
-            <button onClick={() => push('/produk')}>Login</button><br />
-            <button onClick={() => handleLogin()}>Login</button><br />
-            <Link href="/auth/register">Ke Halaman Register</Link>
+            <button onClick={handleLogin}>Login ke halaman produk (Imperatif)</button> <br />
+            {/* <button onClick={() => push('/produk')}>Login</button><br />
+            <button onClick={() => handleLogin()}>Login</button><br /> */}
+            <Link href="/auth/register">Ke Halaman Register (Link)</Link>
         </div>
     );
 };
