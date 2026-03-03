afd78d2ae3457aae4e5352b45fa05f50037c92d0
latif <mabdullatifm1127@gmail.com>
2026-03-03 21:39:08 +0700

    langkah *

--- src/pages/produk/index.tsx
+++ src/pages/produk/index.tsx
@@ -1,9 +1,18 @@
+import { useRouter } from "next/router";
+import {useEffect, useState} from "react";
+
 const produk = () => {
+    const [isLogin, setIsLogin] = useState(false);
+    const {push} = useRouter();
+
+    useEffect(() => {
+        if (!isLogin) {
+            push("/auth/login");
+        }
+    },[]);
     return (
-        <div>
-          <h1>Halaman Produk</h1>
-        </div>
+        <div>ProdukUser Page</div>
     );
-}
+};
 
 export default produk;
